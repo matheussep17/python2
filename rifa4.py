1012011b29ff1db62cf90dc1702920669c34ff41
matheussep17 <matheussep17@gmail.com>
2023-05-28 17:39:51 -0300

    mais melhorias

--- rifa4.py
+++ rifa4.py
@@ -1,18 +1,8 @@
-
 import random
 import sqlite3
 import tkinter as tk
 from tkinter import messagebox
-
-
-# Função para limpar rifas
-def limpar_rifas():
-    conn = sqlite3.connect("rifas.db")
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM rifas")
-    conn.commit()
-    conn.close()
-    messagebox.showinfo("Sucesso", "Todas as rifas foram limpas com sucesso!")
+from tkinter import ttk
 
 # Função para criar a tabela de rifas no banco de dados
 def criar_tabela_rifas():
@@ -107,7 +97,18 @@ def exibir_informacoes():
         numero_rifa, nome, telefone, endereco = rifa
         messagebox.showinfo("Informações da Rifa", f"Número da rifa: {numero_rifa}\nNome: {nome}\nTelefone: {telefone}\nEndereço: {endereco}")
     else:
-        messagebox.showinfo("Informações da Rifa", f"A rifa número {numero_rifa} não foi vendida.")
+        messagebox.showinfo("Informações da Rifa", f"A rifa número {numero_rifa} não foi encontrada.")
+
+# Função para limpar as rifas
+def limpar_rifas():
+    result = messagebox.askyesno("Limpar Rifas", "Tem certeza que deseja limpar todas as rifas?")
+    if result == tk.YES:
+        conn = sqlite3.connect("rifas.db")
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM rifas")
+        conn.commit()
+        conn.close()
+        messagebox.showinfo("Limpar Rifas", "Todas as rifas foram removidas do banco de dados.")
 
 # Cria a tabela de rifas no banco de dados (executar somente na primeira execução)
 criar_tabela_rifas()
@@ -115,52 +116,54 @@ criar_tabela_rifas()
 # Cria a janela principal
 window = tk.Tk()
 window.title("Sistema de Venda de Rifas")
+window.geometry("400x400")
+
+# Define um estilo para os widgets
+style = ttk.Style()
+style.configure("TButton", padding=10, font=("Arial", 12))
+style.configure("TLabel", padding=10, font=("Arial", 12))
+style.configure("TEntry", padding=10, font=("Arial", 12))
 
 # Cria os widgets da interface
-numero_rifa_label = tk.Label(window, text="Número da Rifa:")
+numero_rifa_label = ttk.Label(window, text="Número da Rifa:")
 numero_rifa_label.pack()
-numero_rifa_entry = tk.Entry(window)
+numero_rifa_entry = ttk.Entry(window)
 numero_rifa_entry.pack()
 
-nome_label = tk.Label(window, text="Nome:")
+nome_label = ttk.Label(window, text="Nome:")
 nome_label.pack()
-nome_entry = tk.Entry(window)
+nome_entry = ttk.Entry(window)
 nome_entry.pack()
 
-telefone_label = tk.Label(window, text="Telefone:")
+telefone_label = ttk.Label(window, text="Telefone:")
 telefone_label.pack()
-telefone_entry = tk.Entry(window)
+telefone_entry = ttk.Entry(window)
 telefone_entry.pack()
 
-endereco_label = tk.Label(window, text="Endereço:")
+endereco_label = ttk.Label(window, text="Endereço:")
 endereco_label.pack()
-endereco_entry = tk.Entry(window)
+endereco_entry = ttk.Entry(window)
 endereco_entry.pack()
 
-numero_rifa_ver_label = tk.Label(window, text="Número da Rifa para Ver Informações:")
-numero_rifa_ver_label.pack()
-numero_rifa_ver_entry = tk.Entry(window)
-numero_rifa_ver_entry.pack()
-
-vender_button = tk.Button(window, text="Vender Rifa", command=vender_rifa)
+vender_button = ttk.Button(window, text="Vender Rifa", command=vender_rifa)
 vender_button.pack()
 
-mostrar_rifas_button = tk.Button(window, text="Mostrar Rifas Vendidas", command=mostrar_rifas)
+mostrar_rifas_button = ttk.Button(window, text="Mostrar Rifas Vendidas", command=mostrar_rifas)
 mostrar_rifas_button.pack()
 
-sortear_rifa_button = tk.Button(window, text="Sortear Rifa", command=sortear_rifa)
+sortear_rifa_button = ttk.Button(window, text="Sortear Rifa", command=sortear_rifa)
 sortear_rifa_button.pack()
 
-exibir_informacoes_button = tk.Button(window, text="Exibir Informações", command=exibir_informacoes)
+numero_rifa_ver_label = ttk.Label(window, text="Número da Rifa para Ver Informações:")
+numero_rifa_ver_label.pack()
+numero_rifa_ver_entry = ttk.Entry(window)
+numero_rifa_ver_entry.pack()
+
+exibir_informacoes_button = ttk.Button(window, text="Exibir Informações", command=exibir_informacoes)
 exibir_informacoes_button.pack()
 
-limpar_rifas_button = tk.Button(window, text="Limpar Rifas", command=limpar_rifas)
+limpar_rifas_button = ttk.Button(window, text="Limpar Rifas", command=limpar_rifas)
 limpar_rifas_button.pack()
 
 # Inicia o loop principal da interface gráfica
 window.mainloop()
-
-
-
-
-
